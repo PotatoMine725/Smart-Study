75b03eecc1a3333eb52cb2b2c5c4c4c1c991f07d
Potatomine725 <WotblitzGamer725@gmail.com>
2026-03-09 11:31:19 +0700

    initialize

---
+++ SmartStudyPlanner/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartStudyPlanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTaoHocKy_Click(object sender, RoutedEventArgs e)
+        {
+            string tenHK = txtTenHocKy.Text;
+            DateTime? ngayChon = dpNgayBatDau.SelectedDate;
+
+            if (string.IsNullOrEmpty(tenHK) || ngayChon == null)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tên học kỳ và ngày bắt đầu", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else {
+                // Lấy ngày ra và format thành chuẩn Ngày/Tháng/Năm của Việt Nam
+                string chuoiNgay = ngayChon.Value.ToString("dd/MM/yyyy");
+
+                // Dùng nội suy chuỗi để ghép cả tên và ngày
+                txtTrangThai.Text = $"Đã tạo học kỳ {tenHK} bắt đầu từ ngày {chuoiNgay}";
+            }
+        }
+    }
+}
